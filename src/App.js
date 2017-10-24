ed82711d1344da7f1b84411ad37fc02eb3fb6f89
adam_giacomelli <adam.giacom@gmail.com>
2017-10-24 22:47:56 +0200

    Added mobx & counter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { observer } from 'mobx-react'
+import DevTools from 'mobx-react-devtools';
 import logo from './images/logo.svg'
 import './App.css'
 
+
 class App extends Component {
   render () {
+    const {counter} = this.props;
     return (
       <div className='App'>
         <header className='App-header'>
@@ -13,9 +17,18 @@ class App extends Component {
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Counter:
+          <span className={counter.isOdd ? 'Counter-odd' : 'Counter-even'}> {counter.count} </span>
+        </p>
+        <p>
+          <button onClick={() => counter.increment()}> + </button>
+          <button onClick={() => counter.decrement()}> - </button>
+        </p>
+        <DevTools />
       </div>
     )
   }
 }
 
-export default App
+export default observer(App)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Counter from './Counter';
 import './index.css'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App counter={new Counter()} />, document.getElementById('root'))
