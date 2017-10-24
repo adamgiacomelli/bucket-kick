6abf0a7a88817c4f7d51f5c3802d0806cf8158b7
adam_giacomelli <adam.giacom@gmail.com>
2017-10-24 23:13:27 +0200

    Minor styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,31 @@ import DevTools from 'mobx-react-devtools';
 import logo from './images/logo.svg'
 import './App.css'
 
+const Button = ({children, btnClick}) =>
+<button style={{
+  color: "white",
+  border: "3px solid white",
+  borderRadius: "12px",
+  textAlign: "center",
+  padding: "10px 30px",
+  fontSize:"26px",
+  fontWeight: "900",
+  outline: "none",
+  cursor: "pointer",
+  background: "linear-gradient(to bottom, rgba(76,76,76,1) 0%,rgba(89,89,89,1) 12%,rgba(102,102,102,1) 25%,rgba(71,71,71,1) 39%,rgba(43,43,43,1) 76%,rgba(17,17,17,1) 90%,rgba(28,28,28,1) 91%,rgba(19,19,19,1) 100%)"
+}
+} onClick={btnClick}>{children}</button>
+
+const CounterStyle = observer(({counter}) => 
+<p
+style={{
+    fontSize: "50px"
+  }}>
+Bucket kicked:
+  <span> {counter.count} </span>
+  times
+</p>
+)
 
 class App extends Component {
   render () {
@@ -12,18 +37,15 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-          <h1 className='App-title'>Welcome to React</h1>
+          <h1 className='App-title'>Welcome to Bucket Kick</h1>
         </header>
         <p className='App-intro'>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+          <CounterStyle counter={counter}/>
         <p>
-          Counter:
-          <span className={counter.isOdd ? 'Counter-odd' : 'Counter-even'}> {counter.count} </span>
-        </p>
-        <p>
-          <button onClick={() => counter.increment()}> + </button>
-          <button onClick={() => counter.decrement()}> - </button>
+          <Button btnClick={() => counter.increment()}> Kick it! </Button>
+          <Button btnClick={() => counter.decrement()}> UnKickIt! </Button>
         </p>
         <DevTools />
       </div>
@@ -31,4 +53,4 @@ class App extends Component {
   }
 }
 
-export default observer(App)
+export default App
